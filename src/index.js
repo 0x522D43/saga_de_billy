f464ed77bc12dae7920ea6df6b385e7abea803e7
0x522D43 <71186795+0x522D43@users.noreply.github.com>
2024-02-11 20:56:05 +0100

    add webpack and refactor

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./template/fdcn-caractere";
+import css from "./style.css";
+
+const style = document.createElement("style")
+style.innerText = css
+document.head.appendChild(style)
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+       {
+         test: /\.css$/i,
+         use: [ "css-loader"],
+       },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+    hash: true,
+    minify: false,
+  })],
+};
